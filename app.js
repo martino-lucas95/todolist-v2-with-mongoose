e5ff3e60efdac6a6823435ed53365270005eaaea
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-10-04 23:15:39 -0300

    mongoose working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,33 +10,70 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/toDoListDB');
+
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todoList!"
+});
+
+const item2 = new Item({
+  name: "Hit the + to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3]
+
+
 
 app.get("/", function(req, res) {
+  
+  Item.find({}, function(err, itemsFound){
+    if(itemsFound.length === 0){
+        Item.insertMany(defaultItems, function(err){
+          if(err){
+            console.log(err);
+          } else { 
+            console.log("Success!");
+          }
+        })
+        res.redirect("/");
+    }
+    else if(err){
+          console.log(err);
+    } else {
+          res.render("list", {listTitle: "Today", newListItems: itemsFound});
+    }
+})
 
-const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
-app.post("/", function(req, res){
+// app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  // const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+// });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
 
 app.get("/about", function(req, res){
   res.render("about");
